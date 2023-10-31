13ebaf5d2e435050703fe3e92604fd0b263d81ec
Paulo Cesar <paulo.cesar97hotmail.com>
2023-10-31 18:47:03 -0300

    move some codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { Response } from './core/models/missingPerson.model';
-import { loadMissingPersons } from './core/store/missingPerson/missingPerson.actions';
-import { getMissingPersonList } from './core/store/missingPerson/missingPerson.selectors';
-import { missingPerson } from './core/store/missingPerson/missingPerson.store';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit
-{
-    public listMissingPerson$: Observable<Response> = this.store.select(getMissingPersonList);
-
-    constructor(private readonly store: Store<missingPerson>)
-    { }
-
-    public ngOnInit(): void
-    {
-        this.store.dispatch(loadMissingPersons());
-    }
-
-    public details(item: number)
-    {
-        console.log(item);
-    }
-
-}
+export class AppComponent
+{ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsModule } from 'src/components/components.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { DetailsModule } from './details/details.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ComponentsModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule,
+    DetailsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
