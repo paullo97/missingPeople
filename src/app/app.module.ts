1de57388c4c9c57b429d4f75d24889711ff27357
Paulo Cesar <paulo.cesar97hotmail.com>
2023-10-31 04:42:25 -0300

    import module core to usage store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentsModule } from 'src/components/components.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ComponentsModule
+    ComponentsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
