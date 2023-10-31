b9d83802c7329b2a10383128d05205783b1702f8
Paulo Cesar <paulo.cesar97hotmail.com>
2023-10-31 04:41:26 -0300

    import some modules to usage

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,16 +1,16 @@
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { ContainerComponent } from './container/container.component';
 import { FilterComponent } from './filter/filter.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-
 
 @NgModule({
     declarations: [
@@ -26,7 +26,10 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
         MatFormFieldModule,
         NgIf,
         MatRadioModule,
-        MatCardModule
+        MatCardModule,
+        MatProgressSpinnerModule,
+        ReactiveFormsModule,
+        CommonModule
     ],
     exports: [
         ToolbarComponent,
