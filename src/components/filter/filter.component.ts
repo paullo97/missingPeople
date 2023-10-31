4192daf81eb9efc1a4c6b7205d73c01144e0c0bc
Paulo Cesar <paulo.cesar97hotmail.com>
2023-10-31 04:40:58 -0300

    refactore some code and implements conection with store ngrx

--- src/components/filter/filter.component.ts
+++ src/components/filter/filter.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filterList, loadMissingPersons } from 'src/app/core/store/missingPerson/missingPerson.actions';
+import { missingPerson } from 'src/app/core/store/missingPerson/missingPerson.store';
 
 @Component({
   selector: 'app-filter',
@@ -7,6 +10,9 @@ import { Component } from '@angular/core';
 })
 export class FilterComponent
 {
+    constructor(private readonly store: Store<missingPerson>)
+    { }
+
     public name: string = '';
     public initialAge: string = '';
     public finalAge: string = '';
@@ -18,22 +24,35 @@ export class FilterComponent
         this.initialAge = '';
         this.finalAge = '';
         this.gender = '';
+
+        this.store.dispatch(loadMissingPersons());
     }
 
     public searchPerson(): void
     {
-        console.log(
-            this.name, this.initialAge, this.finalAge, this.gender
-        );
-
-        if(this.name === '' && this.initialAge === '' && this.finalAge === '' && this.gender === '')
+        if(!this.name.length && !this.initialAge.length && !this.finalAge.length && !this.gender.length)
         {
-            console.log('FIX ME -> You not search with parameters empty')
+            console.log('FIX ME -> You not search with parameters empty');
+            return;
         }
 
         if(this.initialAge > this.finalAge)
         {
-            console.log("FIX ME -> You not set up initial age smaller to final age")
+            if(this.finalAge.length !== 0)
+            {
+                console.log("FIX ME -> You not set up initial age smaller to final age");
+                return;
+            }
         }
+
+        this.store.dispatch(filterList({
+            filter: {
+                ...(this.name.length !== 0 && { name: this.name }),
+                ...(this.initialAge.length !== 0 && { minimumAge: this.initialAge }),
+                ...(this.finalAge.length !== 0 && { maximumAge: this.finalAge }),
+                ...(this.gender.length !== 0 && { gender: this.gender })
+            }
+        }));
+
     }
 }
