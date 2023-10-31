751542bd761d5a58785a73b7e0e58eb0d81bb397
Paulo Cesar <paulo.cesar97hotmail.com>
2023-10-31 04:43:13 -0300

    implements function to get list missing persons and function to output when click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Response } from './core/models/missingPerson.model';
+import { loadMissingPersons } from './core/store/missingPerson/missingPerson.actions';
+import { getMissingPersonList } from './core/store/missingPerson/missingPerson.selectors';
+import { missingPerson } from './core/store/missingPerson/missingPerson.store';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit
+{
+    public listMissingPerson$: Observable<Response> = this.store.select(getMissingPersonList);
+
+    constructor(private readonly store: Store<missingPerson>)
+    { }
+
+    public ngOnInit(): void
+    {
+        this.store.dispatch(loadMissingPersons());
+    }
+
+    public details(item: number)
+    {
+        console.log(item);
+    }
+
+}
